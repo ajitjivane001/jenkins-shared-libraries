35395c19320f225c29c9169ea3f29ac54fb51f5b
ajitjivane001 <ajitjivane65@gmail.com>
2026-04-20 22:29:07 +0530

    Rename parameters in clone function

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,4 +1,4 @@
-def call(String repoUrl, String branchName) {
+def call(String url, String branch) {
     echo "Cloning repository from shared library..."
 
     git url: https://github.com/ajitjivane001/django-notes-app.git, branch: main
