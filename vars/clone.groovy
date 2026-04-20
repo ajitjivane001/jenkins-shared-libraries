ddd6dbfc39cd260b13c1673b2e68852cbde5baee
ajitjivane001 <ajitjivane65@gmail.com>
2026-04-20 22:36:29 +0530

    Parameterize git clone URL and branch

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
 def call(String url, String branch) {
     echo "Cloning repository from shared library..."
-
-    git url: https://github.com/ajitjivane001/django-notes-app.git, branch: main
+ git url: url, branch: branch
+  
 }
